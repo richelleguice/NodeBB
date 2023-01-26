0182da59f3de888f8e4e01808b05c0adcc47d298
Richelle Guice <rguice@andrew.cmu.edu>
2023-01-26 17:36:23 -0500

    passed all local linter checks

--- src/topics/suggested.js
+++ src/topics/suggested.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,50 +8,37 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const _ = require('lodash');
-const db = require('../database');
-const user = require('../user');
-const privileges = require('../privileges');
-const search = require('../search');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const lodash_1 = __importDefault(require("lodash"));
+const database_1 = __importDefault(require("../database"));
+const user_1 = __importDefault(require("../user"));
+const privileges_1 = __importDefault(require("../privileges"));
+const search_1 = __importDefault(require("../search"));
 module.exports = function (Topics) {
-    Topics.getSuggestedTopics = function (tid, uid, start, stop, cutoff = 0) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let tids, tid_num;
-            tid_num = parseInt(tid, 10);
-            cutoff = cutoff === 0 ? cutoff : (cutoff * 2592000000);
-            const [tagTids, searchTids] = yield Promise.all([
-                getTidsWithSameTags(tid_num, cutoff),
-                getSearchTids(tid_num, uid, cutoff),
-            ]);
-            tids = _.uniq(tagTids.concat(searchTids));
-            let categoryTids = [];
-            if (stop !== -1 && tids.length < stop - start + 1) {
-                categoryTids = yield getCategoryTids(tid_num, cutoff);
-            }
-            tids = _.shuffle(_.uniq(tids.concat(categoryTids)));
-            tids = yield privileges.topics.filterTids('topics:read', tids, uid);
-            let topicData = yield Topics.getTopicsByTids(tids, uid);
-            topicData = topicData.filter((topic) => topic && topic.tid !== tid);
-            topicData = yield user.blocks.filter(uid, topicData);
-            topicData = topicData.slice(start, stop !== -1 ? stop + 1 : undefined)
-                .sort((t1, t2) => t2.timestamp - t1.timestamp);
-            return topicData;
-        });
-    };
     function getTidsWithSameTags(tid, cutoff) {
         return __awaiter(this, void 0, void 0, function* () {
             const tags = yield Topics.getTopicTags(tid);
-            let tids = cutoff === 0 ?
-                yield db.getSortedSetRevRange(tags.map((tag) => `tag:${tag}:topics`), 0, -1) :
-                yield db.getSortedSetRevRangeByScore(tags.map((tag) => `tag:${tag}:topics`), 0, -1, '+inf', Date.now() - cutoff);
+            let tids;
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            tids = cutoff === 0 ?
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                yield database_1.default.getSortedSetRevRange(tags.map((tag) => `tag:${tag}:topics`), 0, -1) :
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                yield database_1.default.getSortedSetRevRangeByScore(tags.map((tag) => `tag:${tag}:topics`), 0, -1, '+inf', Date.now() - cutoff);
             tids = tids.filter((_tid) => _tid !== tid); // remove self
-            return _.shuffle(_.uniq(tids)).slice(0, 10).map(Number);
+            return lodash_1.default.shuffle(lodash_1.default.uniq(tids)).slice(0, 10).map(Number);
         });
     }
     function getSearchTids(tid, uid, cutoff) {
         return __awaiter(this, void 0, void 0, function* () {
             const topicData = yield Topics.getTopicFields(tid, ['title', 'cid']);
-            const data = yield search.search({
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            const data = yield search_1.default.search({
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
                 query: topicData.title,
                 searchIn: 'titles',
                 matchWords: 'any',
@@ -61,17 +48,51 @@ module.exports = function (Topics) {
                 timeRange: cutoff !== 0 ? cutoff / 1000 : 0,
                 timeFilter: 'newer',
             });
-            data.tids = data.tids.filter((_tid) => _tid !== tid); // remove self
-            return _.shuffle(data.tids).slice(0, 10).map(Number);
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            data.tids = data.filter((_tid) => _tid !== tid); // remove self
+            return lodash_1.default.shuffle(data.tids).slice(0, 10).map(Number);
         });
     }
     function getCategoryTids(tid, cutoff) {
         return __awaiter(this, void 0, void 0, function* () {
             const cid = yield Topics.getTopicField(tid, 'cid');
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
             const tids = cutoff === 0 ?
-                yield db.getSortedSetRevRange(`cid:${cid}:tids:lastposttime`, 0, 9) :
-                yield db.getSortedSetRevRangeByScore(`cid:${cid}:tids:lastposttime`, 0, 9, '+inf', Date.now() - cutoff);
-            return _.shuffle(tids.map(Number).filter((_tid) => _tid !== tid));
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                yield database_1.default.getSortedSetRevRange(`cid:${cid}:tids:lastposttime`, 0, 9) :
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+                yield database_1.default.getSortedSetRevRangeByScore(`cid:${cid}:tids:lastposttime`, 0, 9, '+inf', Date.now() - cutoff);
+            return lodash_1.default.shuffle(tids.map(Number).filter((_tid) => _tid !== tid));
         });
     }
+    Topics.getSuggestedTopics = function (tid, uid, start, stop, cutoff = 0) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let tids;
+            cutoff = cutoff === 0 ? cutoff : (cutoff * 2592000000);
+            const [tagTids, searchTids] = yield Promise.all([
+                getTidsWithSameTags(tid, cutoff),
+                getSearchTids(tid, uid, cutoff),
+            ]);
+            tids = lodash_1.default.uniq(tagTids.concat(searchTids));
+            let categoryTids = [];
+            if (stop !== -1 && tids.length < stop - start + 1) {
+                categoryTids = yield getCategoryTids(tid, cutoff);
+            }
+            tids = lodash_1.default.shuffle(lodash_1.default.uniq(tids.concat(categoryTids)));
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            tids = yield privileges_1.default.topics.filterTids('topics:read', tids, uid);
+            let topicData = yield Topics.getTopicsByTids(tids, uid);
+            topicData = topicData.filter((topic) => topic && topic.tid !== tid);
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            topicData = (yield user_1.default.blocks.filter(uid, topicData));
+            topicData = topicData.slice(start, stop !== -1 ? stop + 1 : undefined)
+                .sort((t1, t2) => new Date(t2.timestamp).valueOf() - new Date(t1.timestamp).valueOf());
+            return topicData;
+        });
+    };
 };
--- src/topics/suggested.ts
+++ src/topics/suggested.ts
@@ -1,51 +1,46 @@
-'use strict';
 
-const _ : _.LoDashStatic = require('lodash');
+import _ from 'lodash';
 
-const db = require('../database');
-const user = require('../user');
-const privileges = require('../privileges');
-const search = require('../search');
+import db from '../database';
+import user from '../user';
+import privileges from '../privileges';
+import search from '../search';
+import { TopicObject } from '../types/topic';
 
-module.exports = function (Topics: { getSuggestedTopics: (tid: string, uid: string, start: number, stop: number, cutoff?: number) => Promise<any>; getTopicsByTids: (arg0: any, arg1: string) => any; getTopicTags: (arg0: number) => any; getTopicFields: (arg0: number, arg1: string[]) => any; getTopicField: (arg0: number, arg1: string) => any; }) {
-    Topics.getSuggestedTopics = async function (tid : string, uid : string, start: number, stop: number, cutoff = 0) {
-        let tids, tid_num;
-        tid_num = parseInt(tid, 10);
-        cutoff = cutoff === 0 ? cutoff : (cutoff * 2592000000);
-        const [tagTids, searchTids] = await Promise.all([
-            getTidsWithSameTags(tid_num, cutoff),
-            getSearchTids(tid_num, uid, cutoff),
-        ]);
+interface Topics {
+    getSuggestedTopics: (tid: number, uid: number, start: number, stop: number, cutoff?: number)
+    => Promise<TopicObject[]>;
+    getTopicsByTids: (arg0: number[], arg1: number) => Promise<TopicObject[]>;
+    getTopicTags: (arg0: number) => Promise<string[]>;
+    getTopicFields: (arg0: TopicObject['tid'], arg1: string[]) => Promise<TopicObject>;
+    getTopicField: (arg0: number, arg1: string) => Promise<string>;
+}
 
-        tids = _.uniq(tagTids.concat(searchTids));
+type DataObject = {
+    tids: number[]
+}
 
-        let categoryTids = [];
-        if (stop !== -1 && tids.length < stop - start + 1) {
-            categoryTids = await getCategoryTids(tid_num, cutoff);
-        }
-        tids = _.shuffle(_.uniq(tids.concat(categoryTids)));
-        tids = await privileges.topics.filterTids('topics:read', tids, uid);
-
-        let topicData = await Topics.getTopicsByTids(tids, uid);
-        topicData = topicData.filter((topic: { tid: string; }) => topic && topic.tid !== tid);
-        topicData = await user.blocks.filter(uid, topicData);
-        topicData = topicData.slice(start, stop !== -1 ? stop + 1 : undefined)
-            .sort((t1: { timestamp: number; }, t2: { timestamp: number; }) => t2.timestamp - t1.timestamp);
-        return topicData;
-    };
-
+export = function (Topics: Topics) {
     async function getTidsWithSameTags(tid: number, cutoff: number) {
         const tags = await Topics.getTopicTags(tid);
-        let tids = cutoff === 0 ?
-            await db.getSortedSetRevRange(tags.map((tag: any) => `tag:${tag}:topics`), 0, -1) :
-            await db.getSortedSetRevRangeByScore(tags.map((tag: any) => `tag:${tag}:topics`), 0, -1, '+inf', Date.now() - cutoff);
-        tids = tids.filter((_tid: number) => _tid !== tid); // remove self
+        let tids : number[];
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        tids = cutoff === 0 ?
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            await db.getSortedSetRevRange(tags.map((tag: string) => `tag:${tag}:topics`), 0, -1) :
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            await db.getSortedSetRevRangeByScore(tags.map((tag: string) => `tag:${tag}:topics`), 0, -1, '+inf', Date.now() - cutoff);
+        tids = tids.filter((_tid : number) => _tid !== tid); // remove self
         return _.shuffle(_.uniq(tids)).slice(0, 10).map(Number);
     }
 
-    async function getSearchTids(tid: number, uid: string, cutoff: number) {
+    async function getSearchTids(tid: number, uid: number, cutoff: number) {
         const topicData = await Topics.getTopicFields(tid, ['title', 'cid']);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const data = await search.search({
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
             query: topicData.title,
             searchIn: 'titles',
             matchWords: 'any',
@@ -54,16 +49,52 @@ module.exports = function (Topics: { getSuggestedTopics: (tid: string, uid: stri
             returnIds: true,
             timeRange: cutoff !== 0 ? cutoff / 1000 : 0,
             timeFilter: 'newer',
-        });
-        data.tids = data.tids.filter((_tid: number) => _tid !== tid); // remove self
+        }) as number[] & DataObject;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        data.tids = data.filter((_tid: number) => _tid !== tid); // remove self
         return _.shuffle(data.tids).slice(0, 10).map(Number);
     }
 
     async function getCategoryTids(tid: number, cutoff: number) {
         const cid = await Topics.getTopicField(tid, 'cid');
-        const tids = cutoff === 0 ?
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const tids: number[] = cutoff === 0 ?
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             await db.getSortedSetRevRange(`cid:${cid}:tids:lastposttime`, 0, 9) :
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             await db.getSortedSetRevRangeByScore(`cid:${cid}:tids:lastposttime`, 0, 9, '+inf', Date.now() - cutoff);
         return _.shuffle(tids.map(Number).filter((_tid: number) => _tid !== tid));
     }
+
+    Topics.getSuggestedTopics = async function (tid : number, uid : number, start: number, stop: number, cutoff = 0) {
+        let tids : number[];
+        cutoff = cutoff === 0 ? cutoff : (cutoff * 2592000000);
+        const [tagTids, searchTids] = await Promise.all([
+            getTidsWithSameTags(tid, cutoff),
+            getSearchTids(tid, uid, cutoff),
+        ]);
+
+        tids = _.uniq(tagTids.concat(searchTids));
+
+        let categoryTids = [];
+        if (stop !== -1 && tids.length < stop - start + 1) {
+            categoryTids = await getCategoryTids(tid, cutoff);
+        }
+        tids = _.shuffle(_.uniq(tids.concat(categoryTids)));
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        tids = await privileges.topics.filterTids('topics:read', tids, uid);
+
+        let topicData: TopicObject[] = await Topics.getTopicsByTids(tids, uid);
+        topicData = topicData.filter((topic: { tid: number; }) => topic && topic.tid !== tid);
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        topicData = await user.blocks.filter(uid, topicData) as TopicObject[];
+        topicData = topicData.slice(start, stop !== -1 ? stop + 1 : undefined)
+            .sort((t1, t2) => new Date(t2.timestamp).valueOf() - new Date(t1.timestamp).valueOf());
+        return topicData;
+    };
 };
